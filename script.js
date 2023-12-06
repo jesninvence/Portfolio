1e4182ef60ebea9a7fc5c37d99b8930524e8b071
jesninvence <jesninvence04@gmail.com>
2023-12-06 09:09:22 +0800

    updating

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-/*================ SEARCH =================*/
+/*================ Mobile - Navbar =================*/
 
-const searchButton = document.getElementById('search-button'),
-    searchClose = document.getElementById('search-close'),
-    searchContent = document.getElementById('search-content')
+const mobileButton = document.getElementById('mobile-button'),
+    mobileClose = document.getElementById('mobile-close'),
+    mobileContent = document.getElementById('mobile-content')
 
-if(searchButton){
-    searchButton.addEventListener('click', () =>{
-        searchContent.classList.add('show-search')
+if(mobileButton){
+    mobileButton.addEventListener('click', () =>{
+        mobileContent.classList.add('show-mobile')
     })
 }
 
-if(searchClose){
-    searchClose.addEventListener('click', () => {
-        searchContent.classList.remove('show-search')
+if(mobileClose){
+    mobileClose.addEventListener('click', () => {
+        mobileContent.classList.remove('show-mobile')
     })
 }
