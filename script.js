4d9d2a8a5f089e253cee8e41331ba4efac0bb3c6
jesninvence <jesninvence04@gmail.com>
2023-12-06 21:22:31 +0800

    updating

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ if(mobileClose){
         mobileContent.classList.remove('show-mobile')
     })
 }
+
+/*================ Send Mail =================*/
+
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+
+    const serviceID = "service_dj00ij6";
+    const templateID = "template_1vyiw15";
+    
+    emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Message Sent Successfully!");
+    })
+    .catch((err) => console.log(err));
+}
