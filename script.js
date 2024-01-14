02d8fb151447139979afcb01867dc7707bc99a0e
Jesnin Vence <jesninvence04@gmail.com>
2024-01-14 22:46:14 +0800

    updating

--- script.js
+++ script.js
@@ -39,3 +39,30 @@ function sendMail() {
     })
     .catch((err) => console.log(err));
 }
+
+
+/*====== SWIPER =======*/
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView : 1,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+});
+
