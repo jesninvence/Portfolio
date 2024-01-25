ffcad73a252316769386c03c3165031473ea4aef
Jesnin Vence <jesninvence04@gmail.com>
2024-01-26 00:27:15 +0800

    updating

--- script.js
+++ script.js
@@ -16,6 +16,18 @@ if(mobileClose){
     })
 }
 
+/*================ marquee =================*/
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
+
 /*================ Send Mail =================*/
 
 function sendMail() {
